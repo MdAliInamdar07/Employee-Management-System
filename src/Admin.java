768465ede2be058ee5ca822a3b6ebcd19700b474
MdAliInamdar07 <inamdar.ali91@gmail.com>
2025-06-24 13:59:11 +0530

    Errors solved

--- src/Admin.java
+++ src/Admin.java
@@ -17,48 +17,57 @@ public class Admin
     }
 
     public void menuForA(){
+        while(true) {
+            System.out.println("Press 1. to insert an employee record.");
+            System.out.println("Press 2. to get records of all employees inserted.");
+            System.out.println("Press 3. to remove an employee record.");
+            System.out.println("Press 4 to exit");
+            input = sc.nextInt();
 
-        System.out.println("Press 1. to insert an employee record.");
-        System.out.println("Press 2. to get records of all employees inserted.");
-        System.out.println("Press 3. to remove an employee record.");
+            switch (input) {
 
-        input = sc.nextInt();
-
-        switch(input){
-
-            case 1:
-                System.out.println("Enter the key");
-                id = sc.nextInt();
-                System.out.println("Enter the name of the Employee");
-                name = sc.nextLine();
-                System.out.println("Enter the department of the Employee");
-                department = sc.nextLine();
-                if(map.containsKey(id)){
-                    System.out.println("This key already exists. Pleaase try again.");
-                }
-                else{
-                    map.put(id, new Admin(name , department));
-                }
-                break;
-
-            case 2 :
-                for(Map.Entry<Integer, Admin>e : map.entrySet()){
-                    System.out.println(e.getKey());
-                    System.out.println(e.getValue());
-
-                }
-                break;
-
-            case 3:
-                if(map.containsKey(id)){
-                    System.out.println("This key already exists. Cannot be removed.");
-                }
-                else{
-                    System.out.println("Enter the Employee ID to be removed");
+                case 1:
+                    System.out.println("Enter the key");
                     id = sc.nextInt();
-                    map.remove(id);
-                }
+                    sc.nextLine();
+                    System.out.println("Enter the name of the Employee");
+                    name = sc.nextLine();
+                    System.out.println("Enter the department of the Employee");
+                    department = sc.nextLine();
+                    if (map.containsKey(id)) {
+                        System.out.println("This key already exists. Pleaase try again.");
+                    } else {
+                        map.put(id, new Admin(name, department));
+                    }
+                    break;
 
+                case 2:
+                    for (Map.Entry<Integer, Admin> e : map.entrySet()) {
+                        int key = e.getKey();
+                        Admin emp = e.getValue();
+                        System.out.println("Employee ID :: " + key);
+                        System.out.println("Name : " + emp.name + " Department : " + emp.department);
+
+                    }
+                    break;
+
+                case 3:
+                    if (map.containsKey(id)) {
+                        System.out.println("Enter the Employee ID to be removed");
+                        id = sc.nextInt();
+                        sc.nextLine();
+                        map.remove(id);
+                        System.out.println("Employee removed. ");
+
+                    }
+                    else {
+                        System.out.println("This key doesn't exist ");
+                    }
+                    break;
+                case 4:
+                    System.out.println("Thank you");
+                    System.exit(0);
+            }
         }
     }
 
--- src/MainMenu.java
+++ src/MainMenu.java
@@ -4,45 +4,50 @@ public class MainMenu extends Admin {
     static Scanner sc = new Scanner(System.in);
 
     public void menuForE(){
+        while(true){
+            System.out.println("Press '1' to check another employee's ID");
+            System.out.println("Press 2 to look at details of employee");
+            System.out.println("Press 3 to exit");
+            choice1 = sc.nextInt();
 
-        System.out.println("Press '1' to check another employee's ID");
-        System.out.println("Press 2 to look at details of employee");
+            switch(choice1){
 
-        choice1 = sc.nextInt();
+                case 1 :
+                    System.out.println("Enter the Employee ID to be checked");
+                    key = sc.nextInt();
+                    if(map.containsKey(key)){
+                        System.out.println("Yes, the key is present");
+                    }
+                    break;
 
-        switch(choice1){
-
-            case 1 :
-                System.out.println("Enter the Employee ID to be checked");
-                key = sc.nextInt();
-                if(map.containsKey(key)){
-                    System.out.println("Yes, the key is present");
-                }
-                break;
-
-            case 2 :
-                System.out.println("Enter the Employee ID for the details to be searched.");
-                key = sc.nextInt();
-                System.out.println("Employee details : " +map.get(key));
-                break;
-        }
+                case 2 :
+                    System.out.println("Enter the Employee ID for the details to be searched.");
+                    key = sc.nextInt();
+                    System.out.println("Employee details : " +map.get(key));
+                    break;
+                case 3 :
+                    System.exit(0);
+            }
     }
 
     public static void main(String []args){
 
         MainMenu ob = new MainMenu();
-        System.out.println("Press 1. to log in as admin");
-        System.out.println("Press 2. to log in as an employee ");
+        while(true) {
+            System.out.println("Press 1. to log in as admin");
+            System.out.println("Press 2. to log in as an employee ");
+            System.out.println("Press 3 to exit");
+            int choice2 = sc.nextInt();
 
-        int choice2 = sc.nextInt();
-
-        if(choice2 == 1){
-            ob.menuForA();
+            if (choice2 == 1) {
+                ob.menuForA();
+            } else if (choice2 == 2) {
+                ob.menuForE();
+            }
+            else if (choice2 == 3) {
+                System.exit(0);
+            }
         }
-        else if(choice2 == 2){
-            ob.menuForE();
-        }
-
     }
 
 }
